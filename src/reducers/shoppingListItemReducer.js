8e1707a3c3c531b75a47244ecae05927f3723fff
Jeremy R <jeremy.rougeau@aletheiainteractive.com>
2020-07-01 05:50:08 +0000

    Automatically backed up by Learn

--- src/reducers/shoppingListItemReducer.js
+++ src/reducers/shoppingListItemReducer.js
@@ -9,10 +9,14 @@ export default function shoppingListItemReducer(
 
 	switch (action.type) {
 		case 'INCREASE_COUNT':
-			return {
-				...state,
-				items: state.items.concat(state.items.length + 1)
-			}
+		  
+		  
+		    console.log('Current state.items length %s', state.items.length);
+      console.log('Updating state.items length to %s', state.items.length + 1);
+      return Object.assign({}, state, {
+        items: state.items.concat(state.items.length + 1)
+      });
+			
 			
 		default:
 console.log('Initial state.items length: %s', state.items.length);
