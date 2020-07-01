930d810a01444519944346aeac1f010fdbb03150
Jeremy R <jeremy.rougeau@aletheiainteractive.com>
2020-07-01 05:45:35 +0000

    Automatically backed up by Learn

--- src/reducers/shoppingListItemReducer.js
+++ src/reducers/shoppingListItemReducer.js
@@ -13,6 +13,7 @@ export default function shoppingListItemReducer(
 				...state,
 				items: state.items.concat(state.items.length + 1)
 			}
+			
 		default:
 console.log('Initial state.items length: %s', state.items.length);
 		      return state;
