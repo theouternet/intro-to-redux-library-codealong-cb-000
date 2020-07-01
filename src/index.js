46379f5a0bddefa564d60434b89b65e8ab749989
Jeremy R <jeremy.rougeau@aletheiainteractive.com>
2020-07-01 05:33:52 +0000

    Automatically backed up by Learn

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@
 
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { createStore } from 'redux'; /* code change */
+import { createStore } from 'redux';
 import shoppingListItemReducer from './reducers/shoppingListItemReducer';
 import App from './App';
 import './index.css';
